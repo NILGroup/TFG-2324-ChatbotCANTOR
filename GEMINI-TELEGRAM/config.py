3fdba1a3a65cce5edab2636d8098fecb974e57bc
mavice07 <mavice07@ucm.es>
2024-05-27 18:37:52 +0200

    Versión final completa

--- GEMINI-TELEGRAM/config.py
+++ GEMINI-TELEGRAM/config.py
@@ -1,2 +1,2 @@
 TELEGRAM_TOKEN = "6775305743:AAG9ms8ikOt_UcEBfNQsws2kkGSZ3YDrsKQ"
-GEMINI_TOKEN = "AIzaSyBFsPMN5rKrReqIML0BdnwnpwIUG5jKQg4"
+GEMINI_TOKEN = "Insertar aquí el token de la api de gemini"
